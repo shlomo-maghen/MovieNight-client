e2ec30a4aec6a0f02bbd9d88e0fd90fbb61b25d4
Shlomo <shlomoshlomoshlomo@gmail.com>
2024-09-05 00:25:44 -0400

    Fix useEffect in room/id page

--- app/room/[id].tsx
+++ app/room/[id].tsx
@@ -24,7 +24,7 @@ export default function RoomScreen() {
     .catch(error => {
       console.log(error);
     });
-  });
+  }, []);
   
   return(
     <View>
--- components/JoinedRoom.tsx
+++ components/JoinedRoom.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function JoinedRoom(props: { id: string }) {
   return (
     <View>
-      <Text> {props.id} ddd </Text>
+      <Text> Room id: {props.id} </Text>
     </View>
   )
 }
